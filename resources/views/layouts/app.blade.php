bce1d4969d6c1be727dcfafec7dd3f1645feb9cb
Felipe Saenz <saenzfelipe2001@gmail.com>
2023-08-02 21:51:34 -0500

    Asignando favicon de la aplicacion

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,6 +7,8 @@
 
         <title>{{ config('app.name', 'Laravel') }}</title>
 
+        <link rel="shorcut icon" href="{{ asset('favicon.ico')}}">
+
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
